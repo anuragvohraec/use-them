8f264fe2aaa08b37562cff9ec5fe4115c048ea3b
Anurag Vohra <53807480+anuragvohraec@users.noreply.github.com>
2022-01-26 11:30:17 +0530

    WIP

--- src/configs.ts
+++ src/configs.ts
@@ -2,7 +2,7 @@ export class UseThemConfiguration{
     static PRESS_VIB=40;
     static DRAG_VIB=30;
     static IMAGE_EDIT_MID_RANGE=100;
-    static IMAGE_EDIT_ZOOM_SENSITIVITY=1;
+    static IMAGE_EDIT_ZOOM_SENSITIVITY=0.05;
     static IMAGE_EDIT_ZOOM_RESPONSE_TIME=300;
     static IMAGE_EDIT_OP_DEFAULT_SIZE=300;
 }
